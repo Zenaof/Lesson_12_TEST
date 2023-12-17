ded5c6030a6ef68a3236f018f5798b7854c58cea
USER PC4 <muravev592@gmail.com>
2023-12-17 17:29:07 +0300

    first commit

---
+++ simple_autotest.py
@@ -0,0 +1,6 @@
+from functions import salary,hello_who
+assert  hello_who('Max') == 'Hello,Max','Hello who error'
+assert  hello_who('Leo') == 'Hello,Leo','Hello who error'
+assert  hello_who('Nikita') == 'Hello,Nikita','Hello who error'
+assert  salary(hours=2, salary_by_hour=1) == 4
+assert  salary(hours=2, salary_by_hour=2) == 8
